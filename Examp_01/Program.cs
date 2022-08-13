0331a77c498b66c698a0925b0d1766d3a4b752e3
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-13 16:10:07 +0300

    Task_01

---
+++ Examp_01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Данно число,проверить кратность 23 и 7.
+Console.WriteLine("Введите число для проверки кратности 7 и 23");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a%7 == 0 && a%23 ==0)
+{
+    Console.WriteLine(" Число " + a + " кратно 7 и 23 ");
+}
+else
+{
+    Console.WriteLine(" Число "  + a + " не кратно 7 и 23 ");
+}
