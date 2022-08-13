d0121dd32ff1ad03743a6bb6d5eaf971cb17817c
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-13 17:09:22 +0300

    Task_02

---
+++ Exemp_02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите координату х,не равную 0");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите координату y,не равную 0");
+int y = int.Parse(Console.ReadLine());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("Первая четверть");
+}
+
+if (x<0 && y>0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+if (x<0 && y<0)
+{
+    Console.WriteLine("Третья четверть");
+}
+if (x>0 && y<0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
