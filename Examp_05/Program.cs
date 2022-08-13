55177cd6bd2f64b5370a03953834ac0ebfa3f715
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-13 19:08:37 +0300

    Task_05

---
+++ Examp_05/Program.cs
@@ -0,0 +1,14 @@
+﻿//Ввозведите число А в натуральную степень В используя цикл
+Console.WriteLine("Введите значение a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b:");
+int b = int.Parse(Console.ReadLine());
+int count = 1;
+int sum = a;
+
+while(count<b)
+{
+    sum = sum * a;
+    count++;
+}
+Console.WriteLine(sum);
