bdba874496047869124964b7a728274a1c1dc8ee
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-13 18:29:43 +0300

    Task_04

---
+++ Exemp_04/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите значение N:");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while(count <= N)
+
+{
+Console.WriteLine(count * count);
+count = count +1;
+}
