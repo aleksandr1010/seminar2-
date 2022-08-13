9cb09180cc33125f8b8cba6c98ba949db6fdb06e
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-13 17:58:28 +0300

    Task_03

---
+++ Examp_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа проверяет пятизначное число на палиндром.
+Console.WriteLine ("Введите число");
+int x = int.Parse(Console.ReadLine());
+
+if(x / 10000 == x % 10 && x / 1000 % 10 == x / 10 % 10)
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else 
+{
+     Console.WriteLine("Число не является полиндромом");
+}
